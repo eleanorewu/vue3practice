431eb2ba598df5b2872fc6e03e5e5a71a559e10f
eleanorewu <yuenwu850823@gmail.com>
2023-10-17 18:21:26 +0800

    v-for & key

--- v-for 迴圈渲染與 key 屬性/src/App.vue
+++ v-for 迴圈渲染與 key 屬性/src/App.vue
@@ -1,9 +1,47 @@
 <template>
-
+	<p 
+	class="text"
+	:key="textContent"
+	>
+	{{ textContent }}
+	</p>
+	<!-- <p 
+	v-for="(value, key, index) in dataList" 
+	:key="value"
+	>
+		<span v-show="key!=='key3'">{{ index }} . {{ key }} : {{ value }}</span>
+	</p> -->
 </template>
 
 <script setup>
- 
+import { ref } from 'vue';
+
+const 
+textContent = ref("你好，這是我的練習")
+const dataList = {
+	key1: "a",
+	key2: "b",
+	key3: "c",
+	key4: "d"
+}
+setTimeout(() =>{
+	textContent.value ="使用vue3+vite開發"
+},2000)
 </script>
 
-<style></style>
+<style>
+.text {
+	font-size: 40px;
+	animation: openin 1s ease-in-out;
+}
+@keyframes openin {
+	0% {
+		opacity: 0;
+		transform: translateY(20px);
+	}
+	100% {
+		opacity: 1;
+		transform: translateY(0px);
+	}
+}
+</style>
